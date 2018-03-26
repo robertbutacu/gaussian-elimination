0f7aa43e2a6155738e22c8323b84cf1c6672fe8f
robertbutacu <robert.butacu@yahoo.com>
2018-03-27 00:08:23 +0300

    isSingular

--- src/main/scala/gaussian/elimination/Solution.scala
+++ src/main/scala/gaussian/elimination/Solution.scala
@@ -2,17 +2,17 @@ package gaussian.elimination
 
 import structures.{Epsilon, Matrix}
 
-case class Solution(matrix: Matrix[Double], b: List[Double], lastPivot: Int, epsilon: Epsilon){
+case class Solution(matrix: Matrix[Double], b: List[Double], lastPivot: Int, epsilon: Epsilon) {
   def isSingular: MatrixType = {
-    if(Math.abs(matrix.rows(lastPivot).max) < epsilon.toNegative10 )
-      Singular
-    else
-      NotSingular
+    val det = (0 until matrix.M).foldRight(1.0)((curr, acc) => matrix.rows(curr)(curr) * acc)
+
+    if( det != 0 ) NotSingular
+    else Singular
   }
 
   def solve(): Option[List[Double]] = {
     isSingular match {
-      case Singular => Some(solveSystem())
+      case Singular => None
       case NotSingular => Some(solveSystem())
     }
   }
@@ -43,11 +43,11 @@ case class Solution(matrix: Matrix[Double], b: List[Double], lastPivot: Int, eps
 
     val zippedWithB = removedZeroes.zip(b.reverse)
 
-    val result = zippedWithB.foldLeft(List.empty[Double]){(acc, curr) =>
+    val result = zippedWithB.foldLeft(List.empty[Double]) { (acc, curr) =>
       val result = curr._2
       val coefficient = curr._1.head
       val restOfEquation = curr._1.tail
-      val leftSideSum = restOfEquation.zip(acc).foldRight(0.0){(curr, acc) =>
+      val leftSideSum = restOfEquation.zip(acc).foldRight(0.0) { (curr, acc) =>
         acc + curr._1 * curr._2
       }
 
